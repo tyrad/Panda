0b2b611c97a7c154ebe12c6e07547d0708950a02
chenchuang <tyrad@qq.com>
2021-03-01 13:57:19 +0800

    init

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", Package.Dependency.Requirement.exact("4.1.0")),
         .package(url: "https://github.com/Moya/Moya.git", Package.Dependency.Requirement.exact("13.0.1")),
     ],
     targets: [
